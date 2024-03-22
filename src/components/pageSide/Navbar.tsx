23f11f99450a42fdb8dd796998a0929aadf94770
Csizmadia László <csizmadia.laszlo@student.ms.sapientia.ro>
2024-03-22 17:43:05 +0200

    fixed nav

--- src/components/pageSide/Navbar.tsx
+++ src/components/pageSide/Navbar.tsx
@@ -7,10 +7,9 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from "next/link";
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Home', href: '#', current: true },
+  { name: 'Themes', href: '#', current: false },
+  { name: 'Leaderboard', href: '#', current: false },
 ]
 
 function classNames(...classes : any) {
