fd12a673d5b5bd74d9742bee0dadf609e36b2668
Salat-Tamas <tamas.salat2004@gmail.com>
2024-03-22 23:43:26 +0200

    Add carousel component and update Lessoncard styling

--- src/app/(site)/lessons/[slug]/page.tsx
+++ src/app/(site)/lessons/[slug]/page.tsx
@@ -1,12 +1,33 @@
 import Lessoncard from '@/components/ui/Lessoncard'
 import React from 'react'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
-type Props = { params: { slug: string } }
+type lessonProps = { 
+  params: { 
+    slug: string
+  }
+ }
 
-const page = ({ params }: Props) => {
+const page = ({ params }: lessonProps) => {
   return (
-    <div className='flex justify-center h-[100vh] p-5 pt-7'>
-      <Lessoncard />
+    <div className='flex justify-center h-[800px] pt-7'>
+      <Carousel className="w-full max-w-[100vw] h-full relative">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+                  <Lessoncard title={"Slide Title"} content='Here goes some lorem ipsum text about the current slide that you are reading at the moment. Please consider helping us out. Thank you in advance!'/>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className='hidden sm:block bottom-0 translate-y-[30vh] left-10'/>
+      <CarouselNext className='absolute hidden sm:block bottom-0 translate-y-[30vh] right-10'/>
+    </Carousel>
     </div>
   )
 }
--- src/components/ui/Lessoncard.tsx
+++ src/components/ui/Lessoncard.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Button, buttonVariants } from '@/components/ui/button'
 
-type Props = {}
+type slide = {
+        title : string
+        content? : string
+        image?: string
+    }
 
-const Lessoncard = (props: Props) => {
+const Lessoncard = ({title, content, image}: slide) => {
   return (
-    <div className='flex flex-row items-center justify-center border-2 border-red-600 w-[200px] h-[600px]'>
-       
+    <div className='flex flex-col items-center justify-start border-2 border-red-600 w-full h-full p-5'>
+       <div className='border-2 border-gray-950 bg-gradient-to-tr from-gray-800 to bg-indigo-600 w-full h-[700px] p-6 rounded-3xl'>
+            <div className='text-3xl font-bold text-white overflow-hidden pb-4 border-b-2 border-gray-800'>
+                <h2>
+                    {title}
+                </h2>
+            </div>
+            <div className='pt-4 text-white'>
+                <p>
+                    {content}
+                </p>
+            </div>
+       </div>
     </div>
   )
 }
