99a5a38475554fe47b8d85b46a26aa06615a331b
Salat-Tamas <tamas.salat2004@gmail.com>
2024-03-23 00:48:04 +0200

    Add loader component to display skeleton cards while data is loading

--- src/components/pageSide/Lessons.tsx
+++ src/components/pageSide/Lessons.tsx
@@ -8,16 +8,17 @@ import React from "react";
 import { getLessons } from "../../../sanity/lib/client";
 
 import useSWR from "swr";
+import { Skeleton } from "../ui/skeleton";
 
 type Props = {};
 
 const Lessons = (props: Props) => {
   const { data, isLoading, error } = useSWR("/lessons", getLessons.bind(null));
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <Loader/>;
   if (error || data === undefined) return <div>Error: {error}</div>;
 
   return (
-    <div className="p-4 grid grid-cols-2 md:grid-cols-3 gap-1">
+    <div className="p-4 grid grid-cols-2 md:grid-cols-3 gap-1 min-h-[90vh]">
       {data.map((card, i) => (
         <Flipcard
           key={i}
@@ -31,3 +32,26 @@ const Lessons = (props: Props) => {
 };
 
 export default Lessons;
+
+const Loader = () => {
+  return <div className="p-4 grid grid-cols-2 md:grid-cols-3 gap-1">
+      <div className="w-[150px] h-[150px] md:h-[300px] md:w-[450px] md:m-2 lg:h-[400px] lg:w-[500px] bg-cover border-[1px] text-white rounded-lg p-4 bg-gradient-to-tr from-indigo-600 to-purple-700 flex flex-col justify-end">
+          <Skeleton className='h-8 w-[120px]'/>
+      </div>  
+      <div className="w-[150px] h-[150px] md:h-[300px] md:w-[450px] md:m-2 lg:h-[400px] lg:w-[500px] bg-cover border-[1px] text-white rounded-lg p-4 bg-gradient-to-tr from-indigo-600 to-purple-700 flex flex-col justify-end">
+          <Skeleton className='h-8 w-[120px]'/>
+      </div>  
+      <div className="w-[150px] h-[150px] md:h-[300px] md:w-[450px] md:m-2 lg:h-[400px] lg:w-[500px] bg-cover border-[1px] text-white rounded-lg p-4 bg-gradient-to-tr from-indigo-600 to-purple-700 flex flex-col justify-end">
+          <Skeleton className='h-8 w-[120px]'/>
+      </div>  
+      <div className="w-[150px] h-[150px] md:h-[300px] md:w-[450px] md:m-2 lg:h-[400px] lg:w-[500px] bg-cover border-[1px] text-white rounded-lg p-4 bg-gradient-to-tr from-indigo-600 to-purple-700 flex flex-col justify-end">
+          <Skeleton className='h-8 w-[120px]'/>
+      </div>  
+      <div className="w-[150px] h-[150px] md:h-[300px] md:w-[450px] md:m-2 lg:h-[400px] lg:w-[500px] bg-cover border-[1px] text-white rounded-lg p-4 bg-gradient-to-tr from-indigo-600 to-purple-700 flex flex-col justify-end">
+          <Skeleton className='h-8 w-[120px]'/>
+      </div>  
+      <div className="w-[150px] h-[150px] md:h-[300px] md:w-[450px] md:m-2 lg:h-[400px] lg:w-[500px] bg-cover border-[1px] text-white rounded-lg p-4 bg-gradient-to-tr from-indigo-600 to-purple-700 flex flex-col justify-end">
+          <Skeleton className='h-8 w-[120px]'/>
+      </div>
+</div>;
+}
