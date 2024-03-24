772bbbea6840b22cbe21021a9a428f92725d3a09
progenor <nero6126@gmail.com>
2024-03-24 04:59:13 +0200

    made some ai shit

--- src/components/pageSide/Chat.tsx
+++ src/components/pageSide/Chat.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+"use client";
 
-type Props = {};
+import { useState } from "react";
+import { useUIState, useActions } from "ai/rsc";
+import type { AI } from "@/app/(site)/actions";
 
-const Chat = (props: Props) => {
-  return <div>Chat</div>;
-};
+export default function Page() {
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useUIState<typeof AI>();
+  const { submitUserMessage } = useActions<typeof AI>();
 
-export default Chat;
+  return (
+    <div>
+      {
+        // View messages in UI state
+        messages.map((message: any) => (
+          <div key={message.id}>{message.display}</div>
+        ))
+      }
+
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          // Add user message to UI state
+          setMessages((currentMessages: any) => [
+            ...currentMessages,
+            {
+              id: Date.now(),
+              display: <div>{inputValue}</div>,
+            },
+          ]);
+
+          // Submit and get response message
+          const responseMessage = await submitUserMessage(inputValue);
+          setMessages((currentMessages: any) => [
+            ...currentMessages,
+            responseMessage,
+          ]);
+
+          setInputValue("");
+        }}
+      >
+        <input
+          placeholder="Send a message..."
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        />
+      </form>
+    </div>
+  );
+}
