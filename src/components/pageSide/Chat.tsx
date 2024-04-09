7a75ed759eb8ecc1fbce1f030e42bf8614fe59ae
Salat-Tamas <tamas.salat2004@gmail.com>
2024-04-09 18:50:28 +0300

    Adjust size of ai assistant icon in Chat.tsx

--- src/components/pageSide/Chat.tsx
+++ src/components/pageSide/Chat.tsx
@@ -25,8 +25,8 @@ export default function Chat() {
               isVisible ? "opacity-0" : "opacity-100"
             } transition-opacity duration-500`}
             src="/assets/images/icon2.png"
-            height={80}
-            width={80}
+            height={70}
+            width={70}
             alt="Ai assistant icon"
           />
         </div>
